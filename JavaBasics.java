4ae0046bf4a412a8cef4cb313c41e34319837dc9
EishaanAgrawal <eishaanagrawal@gmail.com>
2026-07-21 00:34:08 +0530

    second commit

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,13 +1,60 @@
 
+// import java.util.*;
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int side = sc.nextInt();
+//         int perimeter  = side * 4 ;
+
+//         System.out.println("perimeter of sqaure is:"+ perimeter );
+
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         int A = 10;
+//         int B = 5;
+//         System.out.println("add="+(A+B));
+//         System.out.println("diff=" +(A-B) );
+//     }
+ 
+// }
+
 import java.util.*;
 
 public class JavaBasics {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int side = sc.nextInt();
-        int perimeter  = side * 4 ;
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
 
-        System.out.println("perimeter of sqaure is:"+ perimeter );
+        //  int a = 10;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
 
+        // int a = 10;
+        // int b= --a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int a = 10;
+        // int b= a--;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int A = 10;
+        // int B = 9;
+        // System.out.println(A==B);
+
+        // int A = 10;
+        // int B = 10;
+        // System.out.println(A==B);
     }
 }
