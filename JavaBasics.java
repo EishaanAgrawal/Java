67c549f9daed295ab79b094078de9284e58864ba
EishaanAgrawal <eishaanagrawal@gmail.com>
2026-07-21 00:32:39 +0530

    second committ

--- JavaBasics.java
+++ JavaBasics.java
@@ -5,9 +5,9 @@ public class JavaBasics {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int side = sc.nextInt();
-        int area = side * side;
+        int perimeter  = side * 4 ;
 
-        System.out.println("area of sqaure is:"+ area);
+        System.out.println("perimeter of sqaure is:"+ perimeter );
 
     }
 }
