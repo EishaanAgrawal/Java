82297e10058b5c1ead6570c5c2df2ec67072ba61
EishaanAgrawal <eishaanagrawal@gmail.com>
2026-07-25 06:29:28 +0530

    Conditional Statements

---
+++ Test.java
@@ -0,0 +1,115 @@
+
+// import java.util.*;
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int side = sc.nextInt();
+//         int perimeter  = side * 4 ;
+
+//         System.out.println("perimeter of sqaure is:"+ perimeter );
+
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         int A = 10;
+//         int B = 5;
+//         System.out.println("add="+(A+B));
+//         System.out.println("diff=" +(A-B) );
+//     }
+ 
+// }
+
+// import java.util.*;
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         // int a = 10;
+//         // int b = ++a;
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         //  int a = 10;
+//         // int b = a++;
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // int a = 10;
+//         // int b= --a;
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // int a = 10;
+//         // int b= a--;
+//         // System.out.println(a);
+//         // System.out.println(b);
+
+//         // int A = 10;
+//         // int B = 9;
+//         // System.out.println(A==B);
+
+//         // int A = 10;
+//         // int B = 10;
+//         // System.out.println(A==B);
+//     }
+// }
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+       
+//     }
+// }
+
+// public class JavaBasics {
+//     public static void main(String args[]){
+//         int A= 10;
+//         int B= 5;
+//         System.out.println(B>A);
+//     }
+// }
+
+
+// public class JavaBasics{
+//     public static void main(String args[]){
+//         // LOGICAL OPERATIONS
+
+//         System.out.println( (3>2) && (5>0) ); 
+//         System.out.println( (3<2) && (5>0) );
+//         System.out.println( (3<2) && (5<0) );
+
+//         System.out.println( (3>2) || (5>0) );
+//         System.out.println( (3<2) || (5>0) );
+//         System.out.println( (3<2) || (5<0) );
+
+//         System.out.println( !(0>5));
+
+//         //ASSIGNMENT OPERATORS
+
+//         int A = 10;
+//         //A = A + 10;
+//         A += 10;
+//         int B = 5;
+//         //B= B-5;
+//         B -=5;
+//         int C = 6;
+//         // C = C*6
+//         C*=6;
+//         int D = 7;
+//         // D = D/7
+//         D /= 7 ;
+//         int E = 9;
+//         //E = E%9
+//         E%= 9;
+//         System.out.println(A);
+//         System.out.println(B);
+//         System.out.println(C);
+//         System.out.println(D);
+//         System.out.println(E);
+//     }
+// }
+
